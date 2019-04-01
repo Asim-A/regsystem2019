545ce3ccaa1ab83dfcfe7ddb7adc65bdd7df50bf
Asim-A <Asimabazi@hotmail.no>
2019-04-01 13:57:29 +0200

    fiksa gitignore slik at den har pom

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module register.system.MMXIX {
     requires javafx.fxml;
     requires javafx.controls;
+    requires org.kordamp.bootstrapfx.core;
 
     opens views to javafx.fxml;
     exports org.AHJ;
--- src/main/java/org/AHJ/RegSys.java
+++ src/main/java/org/AHJ/RegSys.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.scene.layout.Panel;
 
 public class RegSys extends Application{
 
@@ -16,6 +17,7 @@ public class RegSys extends Application{
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("/views/MainView.fxml"));
+        loader.setLocation(getClass().getResource("org/kordamp/bootstrapfx/bootstrapfx.css"));
         Parent root = loader.load();
         stage.setScene(new Scene(root));
         stage.setTitle("AHJ");

0a57069c707d9b636b10def1baf896a861be3f3b
Asim-A <asimabazi@hotmail.no>
2019-04-03 23:23:12 +0200

    kjør

---
+++ src/main/java/org/AHJ/models/objekter/Kunder.java
@@ -0,0 +1,13 @@
+package org.AHJ.models.objekter;
+
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Kunder implements Serializable {
+
+    private List<Kunde> kundeListe;
+
+
+
+}

c80b4bfce9568e5f813bd2ce594051bcd8515109
Asim-A <Asimabazi@hotmail.no>
2019-04-03 13:11:36 +0200

    Tenker å bruke List for å lagre flere elementer, hovedsakelig av typen arraylist.

--- src/main/java/org/AHJ/models/Kunde.java
+++ src/main/java/org/AHJ/models/Kunde.java
@@ -1,10 +1,16 @@
 package org.AHJ.models;
 
+import org.AHJ.models.forsikringer.Forsikring;
+
 import java.util.Calendar;
+import java.util.List;
 
 public class Kunde {
 
     private String navn;
     private Calendar calendar;
-    
+    private List<Skademelding> meldinger;
+    private List<Forsikring> forsikringer;
+
+
 }

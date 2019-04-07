047915e09e89226b22aaa82315401b837846fbb3
Asim-A <asimabazi@hotmail.no>
2019-04-07 02:44:22 +0200

    la til midlertidige constructors

--- src/main/java/org/AHJ/models/forsikringer/Båtforsikring.java
+++ src/main/java/org/AHJ/models/forsikringer/Båtforsikring.java
@@ -1,4 +1,8 @@
 package org.AHJ.models.forsikringer;
 
 public class Båtforsikring extends Forsikring{
+    //TODO Oppdater denne modellen
+    public Båtforsikring(double forsikringspremie, double forsikringsbeløp, String forsikringsbetingelser) {
+        super(forsikringspremie, forsikringsbeløp, forsikringsbetingelser);
+    }
 }
--- src/main/java/org/AHJ/models/forsikringer/Reiseforsikring.java
+++ src/main/java/org/AHJ/models/forsikringer/Reiseforsikring.java
@@ -1,4 +1,8 @@
 package org.AHJ.models.forsikringer;
 
 public class Reiseforsikring extends Forsikring {
+    //TODO oppdater denne modellen
+    public Reiseforsikring(double forsikringspremie, double forsikringsbeløp, String forsikringsbetingelser) {
+        super(forsikringspremie, forsikringsbeløp, forsikringsbetingelser);
+    }
 }

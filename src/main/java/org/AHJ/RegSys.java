544daebe030673aea461b61582291d7f4b9a905f
Asim-A <asimabazi@hotmail.no>
2019-04-07 02:38:36 +0200

    liten endring

--- src/main/java/org/AHJ/RegSys.java
+++ src/main/java/org/AHJ/RegSys.java
@@ -15,11 +15,10 @@ public class RegSys extends Application{
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("/views/TESTING.fxml"));
+        /*loader.setLocation(getClass().getResource("/views/TESTING.fxml"));*/
         /*loader.setLocation(getClass().getResource("/views/KundeInfoScene.fxml"));*/
-        /*loader.setLocation(getClass().getResource("/views/Intro.fxml"));*/
-       // loader.setLocation(getClass().getResource("/views/Intro.fxml"));
-        // loader.setLocation(getClass().getResource("/views/KundeOversikt.fxml"));
+        loader.setLocation(getClass().getResource("/views/Intro.fxml"));
+        /*loader.setLocation(getClass().getResource("/views/KundeOversikt.fxml"));*/
         Parent root = loader.load();
         root.getStylesheets().add("https://fonts.googleapis.com/css?family=Roboto");
         root.getStylesheets().add("/views/nykunde.css");

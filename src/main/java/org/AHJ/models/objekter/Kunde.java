aa454dfd60fb5216811ee132452e7db89d4132a4
Asim-A <asimabazi@hotmail.no>
2019-04-07 23:22:41 +0200

    oppdatert modell

--- src/main/java/org/AHJ/models/objekter/Kunde.java
+++ src/main/java/org/AHJ/models/objekter/Kunde.java
@@ -4,6 +4,7 @@ import org.AHJ.models.skjermaer.Skademelding;
 import org.AHJ.models.forsikringer.Forsikring;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
@@ -11,23 +12,32 @@ public class Kunde extends Person implements Serializable {
 
     private Calendar calendar;
     private String fakturaadresse;
-    private String forsikringsnummer;
+    private int forsikringsnummer;
     private List<Forsikring> forsikringer;
-    private List<Skademelding> meldinger;
+    private List<Skademelding> skademeldinger;
     private String ubetalte_erstatninger; //TODO usikker på type, må granskes.
 
-    public Kunde(String fornavn, String etternavn, String fakturaadresse, String forsikringsnummer) {
+    public Kunde(String fornavn, String etternavn, String fakturaadresse, int forsikringsnummer) {
         super(fornavn, etternavn);
         this.fakturaadresse = fakturaadresse;
         this.forsikringsnummer = forsikringsnummer;
+        forsikringer = new ArrayList<Forsikring>();
+        skademeldinger = new ArrayList<Skademelding>();
         calendar = Calendar.getInstance();
+    }
+
+    public void addForsikring(Forsikring forsikring){
+        forsikringer.add(forsikring);
+    }
+    public void addSkademelding(Skademelding skademelding){
+        skademeldinger.add(skademelding);
     }
 
     public Calendar getCalendar() {
         return calendar;
     }
-
     public void setCalendar(Calendar calendar) {
         this.calendar = calendar;
     }
+
 }

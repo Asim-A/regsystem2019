af861d4adf489a8b14d92875e4105a054234a9bf
Asim-A <asimabazi@hotmail.no>
2019-05-01 01:52:25 +0200

    ånei ligger bak innser jeg nå

--- src/main/java/org/AHJ/controllers/FXMLControllers/KundeInfoController.java
+++ src/main/java/org/AHJ/controllers/FXMLControllers/KundeInfoController.java
@@ -5,8 +5,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import org.AHJ.controllers.Handlers.ForsikringerTableViewsHandler;
+import org.AHJ.modeller.forsikringer.Forsikring;
 import org.AHJ.modeller.objekter.Kunde;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class KundeInfoController {
 
@@ -14,6 +20,8 @@ public class KundeInfoController {
     Tab forsikringTab;
     @FXML
     TabPane forsikringerTabPane;
+    @FXML
+    TableView<Forsikring> båtView, fritidsBoligView, hoiView, reiseView;
 
     Kunde kunde;
     ForsikringerTableViewsHandler handler;
@@ -25,12 +33,30 @@ public class KundeInfoController {
     @FXML
     public void initialize(){
 
-        handler = new ForsikringerTableViewsHandler(kunde, forsikringTab, forsikringerTabPane);
+        handler = new ForsikringerTableViewsHandler(
+                kunde,
+                forsikringTab,
+                forsikringerTabPane,
+                hentKolonner(båtView),
+                hentKolonner(fritidsBoligView),
+                hentKolonner(hoiView),
+                hentKolonner(reiseView));
+
 
     }
 
     public void exitApplication(ActionEvent actionEvent) {
         Platform.exit();
+    }
+
+    public List<TableColumn<Forsikring, ?>> hentKolonner(TableView<Forsikring> view){
+        List<TableColumn<Forsikring, ?>> kolonneListe = new ArrayList<>();
+
+        for(TableColumn<Forsikring, ?> kolonne : view.getColumns()){
+            kolonneListe.add(kolonne);
+        }
+
+        return kolonneListe;
     }
 
     public Kunde getKunde() {
--- src/main/java/org/AHJ/controllers/Handlers/ForsikringerTableViewsHandler.java
+++ src/main/java/org/AHJ/controllers/Handlers/ForsikringerTableViewsHandler.java
@@ -4,6 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
 import org.AHJ.modeller.forsikringer.Forsikring;
 import org.AHJ.modeller.objekter.Kunde;
 
@@ -15,7 +16,27 @@ public class ForsikringerTableViewsHandler {
     List<Forsikring> forsikringList;
     Tab forsikringsTab;
     TabPane forsikringerTabPane;
+    List<TableColumn<Forsikring, ?>> båtForsikringKolonner;
+    List<TableColumn<Forsikring, ?>> fritidsBoligForsikringKolonner;
+    List<TableColumn<Forsikring, ?>> hoiForsikringKolonner;
+    List<TableColumn<Forsikring, ?>> reiseForsikringKolonner;
     ObservableList<Forsikring> forsikringObservableList = FXCollections.observableArrayList();
+
+    public ForsikringerTableViewsHandler(Kunde kunde,
+                                         Tab forsikringsTab,
+                                         TabPane forsikringerTabPane,
+                                         List<TableColumn<Forsikring, ?>> båtForsikringKolonner,
+                                         List<TableColumn<Forsikring, ?>> fritidsBoligForsikringKolonner,
+                                         List<TableColumn<Forsikring, ?>> hoiForsikringKolonner,
+                                         List<TableColumn<Forsikring, ?>> reiseForsikringKolonner) {
+        this.kunde = kunde;
+        this.forsikringsTab = forsikringsTab;
+        this.forsikringerTabPane = forsikringerTabPane;
+        this.båtForsikringKolonner = båtForsikringKolonner;
+        this.fritidsBoligForsikringKolonner = fritidsBoligForsikringKolonner;
+        this.hoiForsikringKolonner = hoiForsikringKolonner;
+        this.reiseForsikringKolonner = reiseForsikringKolonner;
+    }
 
     public ForsikringerTableViewsHandler(Kunde kunde, Tab forsikringsTab, TabPane forsikringerTabPane) {
         this.kunde = kunde;
